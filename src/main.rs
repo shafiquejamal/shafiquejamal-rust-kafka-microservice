451454f804eb03ce9fb392a67122926232be62a4
Shafique Jamal <shafique.jamal@gmail.com>
2022-10-22 14:25:26 -0400

    Support getting configuration from environment variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use user_service::{kafka_consumer::IngestConsumer, kafka_producer::create_kafka_producer};
 
 const DEFAULT_BROKERS: &str = "localhost:29092";
@@ -6,14 +8,21 @@ const DEFAULT_LISTEN_TOPIC: &str = "from_router";
 
 #[tokio::main]
 async fn main() {
-    // take brokers and top to listen to, from arg, or environment, or default
+    // take brokers and topics to listen to, from arg, or environment, or default
     let mut args: Vec<String> = std::env::args().collect();
     args.remove(0);
 
-    let brokers = args.pop().unwrap_or(DEFAULT_BROKERS.to_string());
-    let group_id = args.pop().unwrap_or(DEFAULT_CONSUMER_GROUP_ID.to_string());
+    let brokers = args
+        .pop()
+        .or(env::var("USER_SERVICE_BROKERS").ok())
+        .unwrap_or(DEFAULT_BROKERS.to_string());
+    let group_id = args
+        .pop()
+        .or(env::var("USER_SERVICE_CONSUMER_GROUP_ID").ok())
+        .unwrap_or(DEFAULT_CONSUMER_GROUP_ID.to_string());
     let listen_topics = args
         .pop()
+        .or(env::var("USER_SERVICE_LISTEN_TOPICS").ok())
         .map(|l_ts| {
             l_ts.split(",")
                 .map(|x| x.to_owned())
@@ -21,7 +30,7 @@ async fn main() {
         })
         .unwrap_or(vec![DEFAULT_LISTEN_TOPIC.to_string()]);
 
-    let producer = create_kafka_producer(DEFAULT_BROKERS).unwrap();
+    let producer = create_kafka_producer(brokers.as_str()).unwrap();
     let ingest_consumer = IngestConsumer::new(brokers, group_id, listen_topics, producer)
         .expect("Failed to create ingest consumer");
     ingest_consumer.run().await;
